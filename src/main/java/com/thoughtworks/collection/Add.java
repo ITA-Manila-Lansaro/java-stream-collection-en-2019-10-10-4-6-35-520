fd83155e0ef88919f7303060034a9397a7b8baec
ROBERT NAJIB LANSANGAN (OPS-IRIS-ISD-OOCLL/MNL) <LANSARO@OOCL.COM>
2019-10-10 18:46:56 +0800

    Passed some unit testing

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,27 +2,62 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+
+        int result = 0;
+        int left = leftBorder;
+        int right =rightBorder;
+        if (leftBorder > rightBorder){
+            left  = rightBorder;
+            right = leftBorder;
+        }
+
+        for (; left <= right; left++){
+                result += left %2 == 0 ? left : 0;
+        }
+
+        return result;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int result = 0;
+        int left = leftBorder;
+        int right =rightBorder;
+        if (leftBorder > rightBorder){
+            left  = rightBorder;
+            right = leftBorder;
+        }
+
+        for (; left <= right; left++){
+            result += left %2 != 0 ? left : 0;
+        }
+
+        return result;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().mapToInt(a -> a * 3 + 2).sum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return (arrayList.stream()
+                .mapToInt(a -> a%2 != 0 ? a * 3 + 2 : a)
+                .boxed().collect(Collectors.toList()));
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .filter(a -> a%2 != 0)
+                .mapToInt(a -> a * 3 + 5)
+                .sum();
     }
 
 //    public double getMedianOfEven(List<Integer> arrayList) {
@@ -30,7 +65,10 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+       return arrayList.stream()
+               .filter(a -> a%2 == 0)
+               .mapToDouble(a -> a)
+               .average().orElse(0);
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
@@ -45,7 +83,9 @@ public class Add {
 //        throw new NotImplementedException();
 //    }
 
-//    public List<Integer> getProcessedList(List<Integer> arrayList) {
-//        throw new NotImplementedException();
-//    }
+    public List<Integer> getProcessedList(List<Integer> arrayList) {
+        return arrayList.stream()
+                .map(a -> a *3)
+                .map()
+    }
 }
--- src/test/java/com/thoughtworks/collection/AddTest.java
+++ src/test/java/com/thoughtworks/collection/AddTest.java
@@ -20,16 +20,16 @@ public class AddTest {
         assertThat(add.getSumOfEvens(rightBorder, leftBorder)).isEqualTo(30);
     }
 
-//    @Test
-//    public void should_get_sum_of_odds() {
-//        //get the sum of odd numbers between leftBorder and rightBorder
-//        int leftBorder = 1;
-//        int rightBorder = 10;
-//
-//        Add add = new Add();
-//        assertThat(add.getSumOfOdds(leftBorder, rightBorder)).isEqualTo(25);
-//        assertThat(add.getSumOfOdds(rightBorder, leftBorder)).isEqualTo(25);
-//    }
+    @Test
+    public void should_get_sum_of_odds() {
+        //get the sum of odd numbers between leftBorder and rightBorder
+        int leftBorder = 1;
+        int rightBorder = 10;
+
+        Add add = new Add();
+        assertThat(add.getSumOfOdds(leftBorder, rightBorder)).isEqualTo(25);
+        assertThat(add.getSumOfOdds(rightBorder, leftBorder)).isEqualTo(25);
+    }
 
     @Test
     public void should_get_triple_and_add_two() {
@@ -55,29 +55,29 @@ public class AddTest {
         assertThat(add.getTripleOfOddAndAddTwo(arrayList)).isEqualTo(resultList);
     }
 
-//    @Test
-//    public void should_get_sum_of_processed_odd() {
-//        //Find the sum of 3 times plus 5 of the odd elements in the array
-//        Integer[] array = new Integer[]{1, 5, 7, 12, 11, 35, 54, 67, 70};
-//        List<Integer> arrayList = Arrays.asList(array);
-//        Add add = new Add();
-//
-//        assertThat(add.getSumOfProcessedOdds(arrayList)).isEqualTo(408);
-//    }
+    @Test
+    public void should_get_sum_of_processed_odd() {
+        //Find the sum of 3 times plus 5 of the odd elements in the array
+        Integer[] array = new Integer[]{1, 5, 7, 12, 11, 35, 54, 67, 70};
+        List<Integer> arrayList = Arrays.asList(array);
+        Add add = new Add();
 
-//    @Test
-//    public void should_process_elements() {
-//        // Array of 3 times the sum of the previous element and the next element in the array
-//        Integer[] array = new Integer[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
-//        List<Integer> arrayList = Arrays.asList(array);
-//
-//        Add add = new Add();
-//
-//        Integer[] result = new Integer[]{12, 24, 36, 48, 60, 72, 84, 96, 108, 120};
-//        List<Integer> resultList = Arrays.asList(result);
-//
-//        assertThat(add.getProcessedList(arrayList)).isEqualTo(resultList);
-//    }
+        assertThat(add.getSumOfProcessedOdds(arrayList)).isEqualTo(408);
+    }
+
+    @Test
+    public void should_process_elements() {
+        // Array of 3 times the sum of the previous element and the next element in the array
+        Integer[] array = new Integer[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+        List<Integer> arrayList = Arrays.asList(array);
+
+        Add add = new Add();
+
+        Integer[] result = new Integer[]{12, 24, 36, 48, 60, 72, 84, 96, 108, 120};
+        List<Integer> resultList = Arrays.asList(result);
+
+        assertThat(add.getProcessedList(arrayList)).isEqualTo(resultList);
+    }
 
 //    @Test
 //    public void should_return_median_of_even() {
